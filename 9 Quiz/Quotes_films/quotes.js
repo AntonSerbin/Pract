2bbb9bf20b96097f92372be437608c26f64b2a23
Пользователь <antons.zn@gmail.com>
2018-08-06 22:04:26 +0300

    added quotes_filmes

---
+++ 9 Quiz/Quotes_films/quotes.js
@@ -0,0 +1,108 @@
+//задания на уровень
+const tasks = [
+ 	task1 = {task:``,
+			sec:30,
+			sizeFormatAnswer: `9rem`,
+			formatAnswer:"",
+			pict:"./Images/quote (1).jpg"},
+	task2 = {task:``,
+ 			formatAnswer:``,
+			sec:30,
+ 			pict:"./Images/quote (2).jpg"},
+	task3 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (3).jpg"},
+	task4 = {task:``,
+			sec:25,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (4).jpg"},
+	task5 = {task:``,
+			sec:25,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (5).jpg"},
+	task6 = {task:``,
+			sec:30,
+			sizeTask:``,
+			sizeFormatAnswer: `7rem`,
+			formatAnswer:``,
+			pict:"./Images/quote (6).jpg"},
+	task7 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (7).jpg"},
+ 	task8 = {task:``,
+			sec:30,
+			sizeFormatAnswer: `9rem`,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (8).jpg"},
+	task9 = {task:``,
+			sec:30,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (9).jpg"},
+	task10 = {task:``,
+			sec:25,
+			sizeFormatAnswer: ``,
+			sizeTask:``,
+			formatAnswer:``,
+			pict:"./Images/quote (10).jpg"}
+];
+	
+
+
+
+const answers = [
+  	task1 = {
+			sec:12, formatAnswer:"",
+			task:"Бетховен",
+			pict:""},
+	task2 = {
+			sec:12, formatAnswer:"",
+ 			task:`Бой с тенью`,
+ 			pict:""},
+	task3 = {
+			sec:12, formatAnswer:"",
+			task:`Ванильное небо`,
+			pict:""},
+	task4 = {
+			sec:12, formatAnswer:"",
+			task:`Форрет Гамп`,
+			pict:""},
+	task5 = {
+			sec:12, formatAnswer:"",
+			task:`Иван Васильевич меняет профессию`,
+			pict:""},
+	task6 = {
+			sec:12, formatAnswer:"",
+			task:`Мистер и миссис Смит`,
+			pict:""},
+	task7 = {
+			sec:12, formatAnswer:"",
+			task:`1+1`,
+			pict:""},
+	task8 = {
+			sec:12, formatAnswer:"",
+			task:`Доктор Стрэндж`,
+			pict:""},
+	task9 = {
+			sec:12, formatAnswer:"",
+			task:`500 дней лета`,
+			pict:""},
+	task10 = {
+			sec:12, formatAnswer:"",
+			task:`Афоня`,
+			pict:""}
+];
+			
+
