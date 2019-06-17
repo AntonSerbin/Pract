494d38d99c5f423edf0958a4a2c6a9bc70fa2b0f
a. serbin <antons.zn@gmail.com>
2019-06-17 19:31:45 +0300

    upgr_clock

--- 12 Clock/clock.js
+++ 12 Clock/clock.js
@@ -1,6 +1,11 @@
 let sec = document.querySelector(".seconds");
 let min = document.querySelector(".minutes");
 
+
+const secLine = document.querySelector('.secLine'); 
+const minLine = document.querySelector('.minLine'); 
+const hourLine = document.querySelector('.hourLine'); 
+
 function setTime() {
 
 const currentTime = new Date;
@@ -8,29 +13,33 @@ let seconds = currentTime.getSeconds();
 let minutes = currentTime.getMinutes();
 let hours = currentTime.getHours();
 
-// if (seconds==0) secLine.classList.remove("transition");
-// if (seconds==1) secLine.classList.add("transition");
-// if (minutes==0) minLine.classList.remove("transition");
-// if (minutes==1) minLine.classList.add("transition");
-// if (hours==0) hourLine.classList.remove("transition");
-// if (hours==1) hourLine.classList.add("transition");
+let secondAngle = (seconds/60*360)-90;
+let minuteAngle = minutes/60*360-90;
+let hourAngle = hours/12*360-90;
 
-// secLine.style.transform = `rotate(${secondAngle}deg)`;
-// minLine.style.transform = `rotate(${minuteAngle}deg)`;
-// hourLine.style.transform = `rotate(${hourAngle}deg)`;
+if (seconds==0) secLine.classList.remove("transition");
+if (seconds==1) secLine.classList.add("transition");
+if (minutes==0) minLine.classList.remove("transition");
+if (minutes==1) minLine.classList.add("transition");
+if (hours==0) hourLine.classList.remove("transition");
+if (hours==1) hourLine.classList.add("transition");
+
+secLine.style.transform = `rotate(${secondAngle}deg)`;
+minLine.style.transform = `rotate(${minuteAngle}deg)`;
+hourLine.style.transform = `rotate(${hourAngle}deg)`;
 console.log(seconds, minutes, hours);
 console.log(currentTime);
 
 };
 
-// function changeData(){
-// 	console.log(this.value, this.name);
-// 	document.documentElement.style.setProperty(`--${this.name}`, this.value);
-// };
+function changeData(){
+	console.log(this.value, this.name);
+	document.documentElement.style.setProperty(`--${this.name}`, this.value);
+};
 
 
-// const inputs = document.querySelectorAll(".footer input");
-// inputs.forEach(input=>input.addEventListener("change", changeData));
+const inputs = document.querySelectorAll(".footer input");
+inputs.forEach(input=>input.addEventListener("change", changeData));
 
 setInterval(()=>setTime(), 1000);
 // setInterval(function(){ alert("Hello"); }, 3000);
